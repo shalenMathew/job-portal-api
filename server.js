e9c4b0346ad57134824861bca15945ce64da2dd0
shalenmj <shalenmj@gmail.ocm>
2025-04-01 10:51:17 +0530

    ibitial mssg

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require("express")
+const mongoose = require("mongoose");
+const app = express()
+
+
+app.use(express.json())
+
+const mongoURI = "mongodb+srv://shalenmj:zItOA9aPR7V2I7FY@job-portal-db.bvhko19.mongodb.net/?retryWrites=true&w=majority&appName=job-portal-db"
+
+mongoose.connect(mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+.then(() => console.log("✅ Connected to MongoDB Atlas"))
+.catch((err) => console.error("❌ MongoDB Connection Error:", err));
+
+app.get("/", (req, res) => {
+    res.send("Welcome to Job Portal API!");
+});
+
+app.listen(3000, () => {
+    console.log("Server is running on http://localhost:3000");
+});
