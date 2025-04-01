b4df950753b4ac89951bbd3840d60dd0d529a849
shalenmj <shalenmj@gmail.ocm>
2025-04-01 12:04:54 +0530

    added route

---
+++ models/jobModels.js
@@ -0,0 +1,27 @@
+
+const mongoose = require("mongoose");
+
+const jobSchema = new mongoose.Schema({
+
+    title:{
+        type:String,
+        required:true
+    },
+
+    description:{
+        type:String,
+        required:true
+    },
+
+    location:{
+        type:String,
+        required:true
+    }
+
+})
+
+const Job = mongoose.model('Job',jobSchema)
+
+module.exports = Job;
+
+
--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require("express")
 const mongoose = require("mongoose");
 const app = express()
+const Job = require("./models/jobModels");
 
 
 app.use(express.json())
+
 
 const MONGO_URI = "mongodb+srv://shalenmj:zItOA9aPR7V2I7FY@job-portal-db.bvhko19.mongodb.net/?retryWrites=true&w=majority&appName=job-portal-db"
 
@@ -14,9 +16,24 @@ mongoose.connect(MONGO_URI, {
 .then(() => console.log("✅ Connected to MongoDB Atlas"))
 .catch((err) => console.error("❌ MongoDB Connection Error:", err));
 
+// fetching main page 
 app.get("/", (req, res) => {
     res.send("Welcome to Job Portal API!");
 });
+
+
+app.get("/jobs", async(req,res) => {
+
+    try {
+        const jobs = await Job.find();  // Fetch all jobs from the database
+        res.status(200).json(jobs);  // Send the jobs as JSON
+    } catch (err) {
+        res.status(500).json({ message: "Error fetching jobs", error: err });
+    }
+
+});
+
+
 
 app.listen(3000, () => {
     console.log("Server is running on http://localhost:3000");
